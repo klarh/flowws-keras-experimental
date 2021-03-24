87c79e4165535d8c039d5cdb7017d0921692f182
Matthew Spellings <mspells@vectorinstitute.ai>
2021-03-24 15:42:17 -0400

    Make PruneNeuralPotentialLayers use keras's clone_model API

--- flowws_keras_experimental/neural_potential/PruneNeuralPotentialLayers.py
+++ flowws_keras_experimental/neural_potential/PruneNeuralPotentialLayers.py
@@ -12,10 +12,51 @@ from .NeuralPotentialDropout import LearnedDropout
 
 LayerDescription = namedtuple('LayerDescription', ['source', 'json', 'weights'])
 
+def find_learnable_dropout_layers(model):
+    for layer in model.layers:
+        if isinstance(layer, keras.models.Model):
+            yield from find_learnable_dropout_layers(layer)
+        elif isinstance(layer, LearnedDropout):
+            yield layer
+
+def get_input_parent_map(model):
+    result = {}
+    result[model.layers[0]] = model
+
+    for layer in model.layers:
+        if isinstance(layer, keras.Model):
+            result.update(get_input_parent_map(layer))
+    return result
+
+def get_desc(layer):
+    layer_json = layer.get_config()
+    return LayerDescription(layer, layer_json, layer.get_weights())
+
 class Pruner:
     INPUT_CLASS_FUNCTIONS = {}
+    PASS_INPUT_CLASS_FUNCTIONS = {}
     OUTPUT_CLASS_FUNCTIONS = {}
-    PASSTHROUGH_CLASS_FUNCTIONS = {}
+    PASS_OUTPUT_CLASS_FUNCTIONS = {}
+
+    @classmethod
+    def register_pass_input(cls, registered_cls):
+        def result(fn):
+            cls.PASS_INPUT_CLASS_FUNCTIONS[registered_cls] = fn
+            # make sure that superclass handlers don't get invoked
+            # if they have an associated input function registered
+            cls.INPUT_CLASS_FUNCTIONS[registered_cls] = lambda *args, **kwargs: None
+            return fn
+        return result
+
+    @classmethod
+    def register_pass_output(cls, registered_cls):
+        def result(fn):
+            cls.PASS_OUTPUT_CLASS_FUNCTIONS[registered_cls] = fn
+            # make sure that superclass handlers don't get invoked
+            # if they have an associated output function registered
+            cls.OUTPUT_CLASS_FUNCTIONS[registered_cls] = lambda *args, **kwargs: None
+            return fn
+        return result
 
     @classmethod
     def register_input(cls, registered_cls):
@@ -31,41 +72,120 @@ class Pruner:
             return fn
         return result
 
+    @staticmethod
+    def _prune(functions, layer, mask):
+        try:
+            target_f = functions[layer.source.__class__]
+            return target_f(layer, mask)
+        except KeyError:
+            pass
+
+        for (target_class, target_f) in functions.items():
+            if isinstance(layer.source, target_class):
+                layer = target_f(layer, mask)
+                return layer
+
     @classmethod
-    def register_passthrough(cls, registered_cls):
-        def result(fn):
-            cls.PASSTHROUGH_CLASS_FUNCTIONS[registered_cls] = fn
-            return fn
-        return result
+    def prune_pass_input(cls, layer, mask):
+        return Pruner._prune(cls.PASS_INPUT_CLASS_FUNCTIONS, layer, mask)
+
+    @classmethod
+    def prune_pass_output(cls, layer, mask):
+        return Pruner._prune(cls.PASS_OUTPUT_CLASS_FUNCTIONS, layer, mask)
 
     @classmethod
     def prune_input(cls, layer, mask):
-        for (target_class, target_f) in cls.INPUT_CLASS_FUNCTIONS.items():
-            if isinstance(layer.source, target_class):
-                layer = target_f(layer, mask)
-                return layer
+        return Pruner._prune(cls.INPUT_CLASS_FUNCTIONS, layer, mask)
 
     @classmethod
     def prune_output(cls, layer, mask):
-        for (target_class, target_f) in cls.OUTPUT_CLASS_FUNCTIONS.items():
-            if isinstance(layer.source, target_class):
-                layer = target_f(layer, mask)
-                return layer
+        return Pruner._prune(cls.OUTPUT_CLASS_FUNCTIONS, layer, mask)
 
     @classmethod
-    def prune_passthrough(cls, layer, mask):
-        for (target_class, target_f) in cls.PASSTHROUGH_CLASS_FUNCTIONS.items():
-            if isinstance(layer.source, target_class):
-                layer = target_f(layer, mask)
-                return layer
+    def mask_children_downward(cls, layer, mask, new_descriptions):
+        children = [new_descriptions.get(n.layer, get_desc(n.layer))
+                   for n in layer.outbound_nodes]
 
-@Pruner.register_passthrough(keras.layers.BatchNormalization)
+        for child in children:
+            new_desc = cls.prune_input(child, mask)
+            if new_desc is not None:
+                (new_desc, _) = new_desc
+                new_descriptions[new_desc.source] = new_desc
+                continue
+
+            new_desc = cls.prune_pass_input(child, mask)
+            if new_desc is not None:
+                (new_desc, new_mask) = new_desc
+                new_descriptions[new_desc.source] = new_desc
+                cls.mask_children_downward(new_desc.source, new_mask, new_descriptions)
+            else:
+                cls.mask_children_downward(child.source, mask, new_descriptions)
+
+    @classmethod
+    def mask_parents_upward(cls, layer, mask, new_descriptions, parent_map):
+        all_layers = [layer]
+        if layer in parent_map:
+            all_layers.append(parent_map[layer])
+
+        parents = []
+        for layer in all_layers:
+            for n in layer.inbound_nodes:
+                if isinstance(n.inbound_layers, list):
+                    parents.extend([new_descriptions.get(layer, get_desc(layer))
+                                    for layer in n.inbound_layers])
+                else:
+                    parents.append(new_descriptions.get(
+                        n.inbound_layers, get_desc(n.inbound_layers)))
+
+        for parent in parents:
+            new_desc = cls.prune_output(parent, mask)
+            if new_desc is not None:
+                (new_desc, _) = new_desc
+                new_descriptions[new_desc.source] = new_desc
+                continue
+
+            new_desc = cls.prune_pass_output(parent, mask)
+            if new_desc is not None:
+                (new_desc, new_mask) = new_desc
+                new_descriptions[new_desc.source] = new_desc
+                cls.mask_parents_upward(new_desc.source, new_mask, new_descriptions, parent_map)
+            else:
+                cls.mask_parents_upward(parent.source, mask, new_descriptions, parent_map)
+
+@Pruner.register_pass_input(keras.layers.BatchNormalization)
+@Pruner.register_pass_output(keras.layers.BatchNormalization)
 def prune_batchnorm(desc, mask):
     new_weights = []
     for w in desc.weights:
         w = w[mask]
         new_weights.append(w)
-    return desc._replace(weights=new_weights)
+    return desc._replace(weights=new_weights), mask
+
+@Pruner.register_pass_input(keras.layers.DepthwiseConv2D)
+def prune_depthconv2d(desc, mask):
+    new_weights = []
+    kernel_shape = None
+    for w in desc.weights:
+        if w.ndim == 4:
+            kernel_shape = w.shape
+            w = w[..., mask, :]
+        elif w.ndim == 1:
+            w = w.reshape((desc.source.depth_multiplier, -1))
+            w = w[:, mask]
+            w = w.reshape((-1,))
+        new_weights.append(w)
+    new_mask = np.arange(kernel_shape[-1]*kernel_shape[-2]).reshape(kernel_shape[-2:])
+    new_mask = new_mask[mask, :]
+    new_mask = new_mask.reshape((-1,))
+    return desc._replace(weights=new_weights), new_mask
+
+@Pruner.register_pass_input(keras.layers.Flatten)
+def prune_flatten(desc, mask):
+    batch_shape = desc.source.input_shape[1:]
+    indices = np.arange(np.product(batch_shape)).reshape(batch_shape)
+    indices = indices[..., mask]
+    new_mask = indices.reshape((-1,))
+    return desc, new_mask
 
 @Pruner.register_input(keras.layers.Dense)
 def prune_dense(desc, mask):
@@ -74,7 +194,15 @@ def prune_dense(desc, mask):
         if w.ndim == 2:
             w = w[mask]
         new_weights.append(w)
-    return desc._replace(weights=new_weights)
+    return desc._replace(weights=new_weights), mask
+
+@Pruner.register_output(keras.layers.InputLayer)
+def prune_input(desc, mask):
+    new_json = copy.deepcopy(desc.json)
+    shape = list(new_json['batch_input_shape'])
+    shape[-1] = len(mask)
+    new_json['batch_input_shape'] = shape
+    return desc._replace(json=new_json), mask
 
 @Pruner.register_output(keras.layers.Dense)
 def prune_dense(desc, mask):
@@ -86,8 +214,8 @@ def prune_dense(desc, mask):
         else:
             w = w[mask]
         new_weights.append(w)
-    new_json['config']['units'] = len(mask)
-    return desc._replace(json=new_json, weights=new_weights)
+    new_json['units'] = len(mask)
+    return desc._replace(json=new_json, weights=new_weights), mask
 
 @Pruner.register_input(keras.layers.Conv2D)
 def prune_conv2d(desc, mask):
@@ -96,7 +224,7 @@ def prune_conv2d(desc, mask):
         if w.ndim == 4:
             w = w[..., mask, :]
         new_weights.append(w)
-    return desc._replace(weights=new_weights)
+    return desc._replace(weights=new_weights), mask
 
 @Pruner.register_output(keras.layers.Conv2D)
 def prune_conv2d(desc, mask):
@@ -108,8 +236,8 @@ def prune_conv2d(desc, mask):
         else:
             w = w[mask]
         new_weights.append(w)
-    new_json['config']['filters'] = len(mask)
-    return desc._replace(json=new_json, weights=new_weights)
+    new_json['filters'] = len(mask)
+    return desc._replace(json=new_json, weights=new_weights), mask
 
 @flowws.add_stage_arguments
 class PruneNeuralPotentialLayers(flowws.Stage):
@@ -120,65 +248,52 @@ class PruneNeuralPotentialLayers(flowws.Stage):
 
     def run(self, scope, storage):
         model = scope['model']
-        model_json = json.loads(model.to_json())
+        parent_map = get_input_parent_map(model)
 
-        layer_descriptions = [
-            LayerDescription(layer, layer_json, layer.get_weights())
-            for (layer, layer_json) in zip(model.layers, model_json['config']['layers'])]
-        last_mask_indices = last_projection_layer = last_output_shape = None
-        mask_name_remaps = {}
-        for i in range(len(layer_descriptions)):
-            if isinstance(layer_descriptions[i].source, LearnedDropout):
-                json_desc = layer_descriptions[i].json
-                weights = layer_descriptions[i].weights[0]
-                probas = tf.math.sigmoid(weights).numpy()
-                sampled_mask = np.random.uniform(0, 1, size=probas.shape) < probas
-                last_mask_indices = np.where(sampled_mask)[0]
+        to_skip, new_descriptions = [], {}
+        for layer in find_learnable_dropout_layers(model):
+            weights = layer.get_weights()[0]
+            probas = tf.math.sigmoid(weights).numpy()
+            sampled_mask = np.random.uniform(0, 1, size=probas.shape) < probas
+            mask = np.where(sampled_mask)[0]
 
-                mask_name_remaps[json_desc['name']] = json_desc['inbound_nodes'][0][0][0]
+            to_skip.append(layer)
+            Pruner.mask_children_downward(layer, mask, new_descriptions)
+            Pruner.mask_parents_upward(layer, mask, new_descriptions, parent_map)
 
-                if last_projection_layer is not None:
-                    desc = Pruner.prune_output(layer_descriptions[last_projection_layer], last_mask_indices)
-                    assert desc is not None
-                    layer_descriptions[last_projection_layer] = desc
+        new_layer_weights = {}
+        nullified_indices = {layer: i for (i, layer) in enumerate(to_skip)}
 
-                    for j in range(last_projection_layer + 1, i):
-                        layer_descriptions[j] = (
-                            Pruner.prune_passthrough(layer_descriptions[j], last_mask_indices) or
-                            layer_descriptions[j])
-            elif last_mask_indices is not None:
-                if isinstance(layer_descriptions[i].source, keras.layers.Flatten):
-                    batch_shape = last_output_shape[1:]
-                    indices = np.arange(np.product(batch_shape)).reshape(batch_shape)
-                    indices = indices[..., last_mask_indices]
-                    last_mask_indices = indices.reshape(-1)
+        def clonefun(layer):
+            result = None
 
-                desc = Pruner.prune_input(layer_descriptions[i], last_mask_indices)
-                if desc is not None:
-                    layer_descriptions[i] = desc
-                    last_mask_indices = None
+            if layer in to_skip:
+                name = 'nullified_learnable_dropout_{}'.format(nullified_indices[layer])
+                result = keras.layers.Lambda(tf.identity, name=name)
+            if layer in new_descriptions:
+                desc = new_descriptions[layer]
+                result = layer.__class__.from_config(desc.json)
+                new_layer_weights[result] = desc.weights
+            elif isinstance(layer, keras.models.Model):
+                old_input_layer = keras.models.Model.layers.fget(layer)[0]
+                new_input_tensor = None
+                if old_input_layer in new_descriptions:
+                    new_input_desc = new_descriptions[old_input_layer]
+                    new_input_layer = old_input_layer.__class__.from_config(new_input_desc.json)
+                    new_input_tensor = new_input_layer.output
+                result = keras.models.clone_model(layer, new_input_tensor, clone_function=clonefun)
 
-            new_json = copy.deepcopy(layer_descriptions[i].json)
-            for layer_instance in new_json['inbound_nodes']:
-                for input_description in layer_instance:
-                    input_description[0] = mask_name_remaps.get(input_description[0], input_description[0])
-            layer_descriptions[i] = layer_descriptions[i]._replace(json=new_json)
+            if result is not None:
+                return result
 
-            if Pruner.prune_output(layer_descriptions[i], [0]) is not None:
-                last_projection_layer = i
+            return layer.__class__.from_config(layer.get_config())
 
-            last_output_shape = layer_descriptions[i].source.output_shape
+        new_model = keras.models.clone_model(model, clone_function=clonefun)
 
-        final_descriptions = [desc for desc in layer_descriptions if not isinstance(desc.source, LearnedDropout)]
-
-        model_json['config']['layers'] = [desc.json for desc in final_descriptions]
-        weights = sum([desc.weights for desc in final_descriptions], [])
-
-        new_model = keras.models.model_from_json(json.dumps(model_json))
+        for (layer, weights) in new_layer_weights.items():
+            layer.set_weights(weights)
 
         if self.arguments['summarize']:
             new_model.summary()
 
-        new_model.set_weights(weights)
-        new_model.compile(optimizer=model.optimizer, loss=scope['loss'], metrics=scope.get('metrics', []))
         scope['model'] = new_model
