486937f56eb4b845a6076b9836dee3a5cd2092b8
Matthew Spellings <mspells@vectorinstitute.ai>
2022-05-18 09:09:35 -0400

    Add memory_fraction argument to InitializeTF

--- flowws_keras_experimental/InitializeTF.py
+++ flowws_keras_experimental/InitializeTF.py
@@ -1,6 +1,11 @@
+import logging
+import subprocess
+
 import flowws
 from flowws import Argument as Arg
 import tensorflow as tf
+
+logger = logging.getLogger(__name__)
 
 @flowws.add_stage_arguments
 class InitializeTF(flowws.Stage):
@@ -13,6 +18,8 @@ class InitializeTF(flowws.Stage):
             help='If False, disable GPUs'),
         Arg('memory_growth', '-m', bool, True,
             help='If True, enable gradual memory growth'),
+        Arg('memory_fraction', '-f', float,
+            help='If given, limit the GPU memory to the given fraction of available memory'),
     ]
 
     def run(self, scope, storage):
@@ -28,8 +35,26 @@ class InitializeTF(flowws.Stage):
                     # Currently, memory growth needs to be the same across GPUs
                     for gpu in gpus:
                         tf.config.experimental.set_memory_growth(gpu, True)
+
+                if self.arguments.get('memory_fraction', None):
+                    frac = self.arguments['memory_fraction']
+
+                    for gpu in gpus:
+                        index = gpu.name.split(':')[-1]
+                        command = [
+                            'nvidia-smi', '--query-gpu=memory.total,memory.reserved',
+                            '--format=csv,nounits,noheader', '-i', index]
+                        output = subprocess.check_output(command).decode()
+                        left, right = map(float, output.split(','))
+                        available = left - right
+                        usable = int(frac*available)
+
+                        config = [tf.config.LogicalDeviceConfiguration(
+                            memory_limit=usable)]
+                        tf.config.set_logical_device_configuration(gpu, config)
+
                 logical_gpus = tf.config.experimental.list_logical_devices('GPU')
                 print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
-            except RuntimeError as e:
+            except (RuntimeError, ValueError) as e:
                 # Memory growth must be set before GPUs have been initialized
-                print(e)
+                logger.warning('Encountered an error, but continuing: {}'.format(e))
